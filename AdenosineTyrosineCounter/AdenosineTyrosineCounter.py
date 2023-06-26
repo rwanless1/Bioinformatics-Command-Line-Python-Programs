b75ab3067ae3509aa40d68129e4ceffd73906102
rwanless1 <123345184+rwanless1@users.noreply.github.com>
2023-06-25 17:20:15 -0700

    Update AdenosineTyrosineCounter.py

--- AdenosineTyrosineCounter/AdenosineTyrosineCounter.py
+++ AdenosineTyrosineCounter/AdenosineTyrosineCounter.py
@@ -14,4 +14,4 @@ dna = input("Enter a dna sequence: ")
 upperDNA = dna.upper()
 coolString = dnaString(upperDNA)
 
-print ('AT content = {0:0.3f}''.format(coolString.getAT()) ) #the {0:0.3f} was used to have 3 decimal places
+print ('AT content = {0:0.3f}'.format(coolString.getAT()) ) #the {0:0.3f} was used to have 3 decimal places
